5c297d5251ee92eab66771e5e568f1af95c06da6
root <root@debarm.(none)>
2007-01-01 01:57:31 +0100

    name

---
+++ mysql/intrusions.sql
@@ -0,0 +1,13 @@
+/*DROP TABLE IF EXISTS `intrusions`;*/
+
+CREATE TABLE IF NOT EXISTS `intrusions` (
+  `room` varchar(25) NOT NULL ,
+  `status` BIT(1) NOT NULL,
+  PRIMARY KEY (`room`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*LOCK TABLES `intrusions` WRITE;*/
+
+INSERT INTO intrusions (room,status) VALUES, ('Camera','0'), ('Cameretta','0'), ('Cucina','0'), ('Sala','0');
+
+UNLOCK TABLES;
