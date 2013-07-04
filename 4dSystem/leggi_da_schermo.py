af4669a2467dc18b5a6556fbd8b8a776e9889c82
Flavio-Tesi <091Ryuk132@gmail.com>
2013-07-04 17:49:38 +0200

    goto_form CONVERTIRE STRINGA ESADECIMALE IN CARATTEREgit add .

--- 4dSystem/leggi_da_schermo.py
+++ 4dSystem/leggi_da_schermo.py
@@ -9,18 +9,59 @@ ser = serial.Serial(
     bytesize=serial.EIGHTBITS
 )  
 
+
+def calcola_checksum (comando, parametro):
+	com = ord(comando)
+	for i in range (0, len(parametro)):
+		com = com ^ ord(parametro[i])
+	com = chr(com)
+	return com
+	
+#	hesa = "%02x" % ord(chr(index))	
+#	print hesa
+#	print hesa[0]
+#	print hesa[1]
+#	stringa = "".join(["\\", "x",hesa[0], hesa[1]])
+
+def create_string_hex (index):
+	
+	stringa = "\\x%x" % index
+	
+	print stringa
+	return stringa
+
+def goto_form (index):
+	
+	ind = create_string_hex (index)
+	
+	lista = ["\x01","\x0A",ind,"\x00","\x00"]
+			
+	ls1 = ord(lista[0])
+	print "ciaooooooo"
+	print lista[0]
+	print ls1
+	print ind
+			
+	
+	for i in range (0, len(lista)):
+		print "%02x" % ord(lista[i])		
+		
+			
+	for i in range (1, len(lista)):
+		ls1 = calcola_checksum(ls1, ord(lista[i]))
+		
+	ls1 = hex(ord(ls1)) 	# DA ESADECIMALE A STRINGA??
+	
+	lista.append(ls1)
+	
+	for i in range (0, len(lista)):
+		print lista[i]		
+
+
 def verifica (lista):
 	if len(lista)>0:
 		if (lista[0],lista[1],lista[2],lista[3],lista[4],lista[5]) == ("1","7","0","5","0","9"):
-			ser.write ("\x43")
-			ser.write ("\x00")
-			ser.write ("\x3F")
-			ser.write ("\x00")
-			ser.write ("\x3F")
-			ser.write ("\x00")
-			ser.write ("\x22")
-			ser.write ("\x00")
-			ser.write ("\x1F")
+			goto_form(1)
 			
 			while True: 
 				s = ser.read(1)
@@ -44,13 +85,6 @@ def verifica (lista):
 		del lista[0:99]
 		return				
 
-
-def calcola_checksum (comando, parametro):
-	com = ord(comando)
-	for i in range (0, len(parametro)):
-		com = com ^ ord(parametro[i])
-	com = chr(com)
-	return com
 		
 				
 def spacchetta(pacchetto):
@@ -77,23 +111,6 @@ while True:
 		if len(pacchetto)>=3:
 			spacchetta(pacchetto)
 			pacchetto=""
-			
-	#		comando = chr(int("0x01", 16))
-	#		par1 = chr(int("0x0A",16))
-	#		par2 = chr(int("0x01",16))
-	#		par3 = chr(int("0x00",16))
-	#		par = "".join([par1,par2,par3])
-	#		checksum = calcola_checksum(comando,par)
-	#		print "%02x" % ord(checksum)
-			
-			ser.write("\x01")
-			ser.write("\x0A")
-			ser.write("\x01")
-			ser.write("\x00")
-			ser.write("\x00")
-			ser.write("\x0A")
-			
-			
 						
 			
 			print "%02x" % ord(ser.read(1))
