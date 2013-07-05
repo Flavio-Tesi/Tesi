a0b35f47f98f7e311763319c5ccbc093aaecd4a3
Flavio Sciacquatori <flavio@nicola.(none)>
2013-07-05 11:53:54 +0200

    goto_form OK

--- 4dSystem/leggi_da_schermo.py
+++ 4dSystem/leggi_da_schermo.py
@@ -3,7 +3,7 @@ import serial
 ser = serial.Serial(
     port='/dev/ttyS4', 
     baudrate=9600, 
-    timeout=0.5,
+    timeout=0.1,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
@@ -16,51 +16,26 @@ def calcola_checksum (comando, parametro):
 		com = com ^ ord(parametro[i])
 	com = chr(com)
 	return com
-	
-#	hesa = "%02x" % ord(chr(index))	
-#	print hesa
-#	print hesa[0]
-#	print hesa[1]
-#	stringa = "".join(["\\", "x",hesa[0], hesa[1]])
-
-def create_string_hex (index):
-	
-	stringa = "\\x%x" % index
-	
-	print stringa
-	return stringa
 
 def goto_form (index):
 	
-	ind = create_string_hex (index)
-	
-	lista = ["\x01","\x0A",ind,"\x00","\x00"]
-			
-	ls1 = ord(lista[0])
-	print "ciaooooooo"
-	print lista[0]
-	print ls1
-	print ind
-			
-	
-	for i in range (0, len(lista)):
-		print "%02x" % ord(lista[i])		
-		
-			
+	lista = ["\x01","\x0A",chr(index),"\x00","\x00"]
+	ls1 = lista[0]	
 	for i in range (1, len(lista)):
-		ls1 = calcola_checksum(ls1, ord(lista[i]))
-		
-	ls1 = hex(ord(ls1)) 	# DA ESADECIMALE A STRINGA??
-	
+		ls1 = calcola_checksum(ls1, lista[i])
 	lista.append(ls1)
-	
 	for i in range (0, len(lista)):
-		print lista[i]		
+		ser.write(lista[i])
+	
 
 
 def verifica (lista):
 	if len(lista)>0:
-		if (lista[0],lista[1],lista[2],lista[3],lista[4],lista[5]) == ("1","7","0","5","0","9"):
+		lista_control1 = ['1','7','0','5','0','9']
+		lista_control2 = ['9','0','5','0','7','1']
+		
+		if lista == lista_control1:
+			print "evvivaaaa"
 			goto_form(1)
 			
 			while True: 
@@ -70,7 +45,7 @@ def verifica (lista):
 			del lista[0:6]
 			return
 							
-		elif (lista[0],lista[1],lista[2],lista[3],lista[4],lista[5]) == ("9","0","5","0","7","1"):		
+		elif lista == lista_control2:		
 			print "OK! USER 2"
 			del lista[0:6]
 			return
@@ -102,6 +77,7 @@ def spacchetta(pacchetto):
 	return 0
 
 pacchetto = ""
+lista = []
 
 while True: 
 	s = ser.read(1) 
@@ -110,9 +86,43 @@ while True:
 	else:
 		if len(pacchetto)>=3:
 			spacchetta(pacchetto)
-			pacchetto=""
-						
+			numero_codice = pacchetto[-2]
 			
-			print "%02x" % ord(ser.read(1))
+			if numero_codice == "\x08":
+				verifica(lista)
+			
+			elif numero_codice == "\x3c":
+				if len(lista)>0:
+					del lista [-1]
+			
+			else:lista.append (numero_codice)
+			
+			pacchetto = ""
+			
+	#		print "%02x" % ord(ser.read(1))
 			
 ser.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
