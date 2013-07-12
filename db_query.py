7efd41d58040e3799df96428458d9c6796edefed
root <root@ubuntu.(none)>
2013-07-13 00:10:23 +0200

    ok

--- db_query.py
+++ db_query.py
@@ -9,11 +9,11 @@ def read_users():
 		
 def read_userCode (i):
 	cur.execute("SELECT code FROM users WHERE id=%d;" %i)
-	return cur.fetchone()
+	return cur.fetchone()[0]
 		
 def read_userName (i):
 	cur.execute("SELECT name FROM users WHERE id=%d;" %i)
-	return cur.fetchone()
+	return cur.fetchone()[0]
 
 def set_userCode (i, code):
 	cur.execute("UPDATE users SET code=%d WHERE id=%d;" %(code,i))
@@ -23,44 +23,44 @@ def read_temperatures():
 	cur.execute ("SELECT * FROM temperatures;")
 	return cur.fetchall()
 		
-def read_temperature(room):
-	cur.execute ("SELECT val FROM temperatures WHERE room=%s;" %room)
-	return cur.fetchone()
+def read_temperature(i):
+	cur.execute ("SELECT val FROM temperatures WHERE room=%s;" %i)
+	return cur.fetchone()[0]
 		
-def set_temperature(room, val):
-	cur.execute ("UPDATE temperatures SET val=%d WHERE room=%s;" %(val,room))
+def set_temperature(i, val):
+	cur.execute ("UPDATE temperatures SET val=%d WHERE room=%d;" %(val,i))
 	db.commit()
 	
 def read_lights():
 	cur.execute ("SELECT * FROM lights;")
 	return cur.fetchall()
 
-def read_light(room):
-	cur.execute ("SELECT status FROM lights WHERE room=%s;" %room)
-	return cur.fetchone()
+def read_light(i):
+	cur.execute ("SELECT status FROM lights WHERE id=%d;" %i)
+	return cur.fetchone()[0]
 	
-def change_light(room):
-	i = read_intrusion(room)
-	if i == 0:
-		i = 1
-	elif i == 1:
-		i = 0
-	cur.execute ("UPDATE lights SET status=%d WHERE room=%s;" %(i,room))
+def change_light(i):
+	x = read_light(i)
+	if x == 0:
+		x = 1
+	elif x == 1:
+		x = 0
+	cur.execute ("UPDATE lights SET status=%d WHERE id=%d;" %(x,i))
 	db.commit()
 	
 def read_intrusions():
 	cur.execute ("SELECT * FROM intrusions;")
 	return cur.fetchall()
 
-def read_intrusion(room):
-	cur.execute ("SELECT status FROM intrusions WHERE room=%s;" %room)
-	return cur.fetchone()
+def read_intrusion(i):
+	cur.execute ("SELECT status FROM intrusions WHERE id=%d;" %i)
+	return cur.fetchone()[0]
 	
-def change_intrusion(room):
-	i = read_intrusion(room)
-	if i == 0:
-		i = 1
-	elif i == 1:
-		i = 0
-	cur.execute ("UPDATE intrusions SET status=%d WHERE room=%s;" %(i,room))
+def change_intrusion(i):
+	x = read_intrusion(i)
+	if x == 0:
+		x = 1
+	elif x == 1:
+		x = 0
+	cur.execute ("UPDATE intrusions SET status=%d WHERE id=%d;" %(x,i))
 	db.commit()
--- intrux.py
+++ intrux.py
@@ -4,24 +4,32 @@ import tornado.web
 import json
 import db_query
 
-class stamp(tornado.web.RequestHandler):
-	def get(self):
-		x = client.stampa() 
-		self.write(x)		
-		testo = self.get_argument('txt')
-		self.write(testo)
-		client.ricicla(testo)
-
 class execute(tornado.web.RequestHandler):
 	def get(self):
 		if self.get_argument('cmd')=="read_users":
-			rtc=db_query.read_users()
-			print "Read users"
-			self.write(json.dumps(rtc))
+			self.write(json.dumps(db_query.read_users()))
+		elif self.get_argument('cmd')=="read_lights":
+			self.write(json.dumps(db_query.read_lights()))
+		elif self.get_argument('cmd')=="change_light":
+			i = int(self.get_argument('id'))
+			db_query.change_light(i)
+		elif self.get_argument('cmd')=="set_usercode":
+			i = int(self.get_argument('id'))
+			if self.get_argument('pwd0')=='0':
+				self.write("INSERIRE UNA PASSWORD VALIDA")
+			elif int(self.get_argument('pwd0'))==int(db_query.read_userCode(i)):
+				new_pwd = self.get_argument('pwd1')
+				if new_pwd == self.get_argument('pwd2'):
+					db_query.set_userCode(i, int(new_pwd))
+					self.write("PASSWORD CORRETTAMENTE MODIFICATA")
+				else:
+					self.write("PASSWORD NON CORRISPONDENTI")
+			else:
+				self.write("PASSWORD ERRATA")
+			
 		
 application = tornado.web.Application([
 	(r"/execute", execute),
-	(r"/stamp", stamp),
 	(r"/(.*)", tornado.web.StaticFileHandler, {"path": ".","default_filename": "index.html"}),
 ])
 
